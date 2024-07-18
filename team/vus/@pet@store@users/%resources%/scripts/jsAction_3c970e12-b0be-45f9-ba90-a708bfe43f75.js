f22b0767955f817ccfb32c213cff18bc6e4417e6
e10688062 <e10688062@INM01924.rockwellcollins.com>
2024-07-18 12:59:01 +0530

    My1stGitProjfromNeoload

---
+++ team/vus/@pet@store@users/%resources%/scripts/jsAction_3c970e12-b0be-45f9-ba90-a708bfe43f75.js
@@ -0,0 +1,10 @@
+﻿var file = new java.io.File("C:\\Users\\E10688062\\Desktop\\Neoload\\outfile.csv");
+var writer = new java.io.FileWriter(file,true);
+
+
+var value1 = context.variableManager.getValue("Extr_sessionid");
+var value2 = context.variableManager.getValue("LoginCheck");
+writer.write(value1+","+value2+"\n");
+//writer.write("Hello");
+writer.close();
+logger.debug("The values have been written to file");
